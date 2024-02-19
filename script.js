dc458d1bdfa8b86a8506eb80893311422124d935
Leonardo Assom <leonardo.assom@gmail.com>
2024-02-19 22:52:52 +0100

    Update Files

---
+++ script.js
@@ -0,0 +1,28 @@
+function submitForm() {
+    // Ottieni i valori inseriti nel modulo
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    // Mostra un messaggio di conferma
+    alert("Grazie per il tuo messaggio, " + name + "!");
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
