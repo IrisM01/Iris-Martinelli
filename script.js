53a8f243d32e5a8f741851d3a39018b30c1b6d60
Leonardo Assom <leonardo.assom@gmail.com>
2024-02-22 11:10:24 +0100

    Update Slideshow

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Funzione per l'invio del modulo
 function submitForm() {
     // Ottieni i valori inseriti nel modulo
     var name = document.getElementById("name").value;
@@ -8,21 +9,95 @@ function submitForm() {
     alert("Grazie per il tuo messaggio, " + name + "!");
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// Funzioni per lo slideshow dei diversi servizi
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+// Slideshow Interni
+let slideIndexInterni = 0;
+showSlidesInterni();
+
+function showSlidesInterni() {
+    let slides = document.getElementsByClassName("mySlidesInterni");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexInterni++;
+    if (slideIndexInterni > slides.length) {
+        slideIndexInterni = 1;
+    }
+    slides[slideIndexInterni - 1].style.display = "block";
+    setTimeout(showSlidesInterni, 2000); // Cambia immagine ogni 2 secondi
+}
+
+// Slideshow Esterni
+let slideIndexEsterni = 0;
+showSlidesEsterni();
+
+function showSlidesEsterni() {
+    let slides = document.getElementsByClassName("mySlidesEsterni");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexEsterni++;
+    if (slideIndexEsterni > slides.length) {
+        slideIndexEsterni = 1;
+    }
+    slides[slideIndexEsterni - 1].style.display = "block";
+    setTimeout(showSlidesEsterni, 2000); // Cambia immagine ogni 2 secondi
+}
+
+// Slideshow Muri
+let slideIndexMuri = 0;
+showSlidesMuri();
+
+function showSlidesMuri() {
+    let slides = document.getElementsByClassName("mySlidesMuri");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexMuri++;
+    if (slideIndexMuri > slides.length) {
+        slideIndexMuri = 1;
+    }
+    slides[slideIndexMuri - 1].style.display = "block";
+    setTimeout(showSlidesMuri, 2000); // Cambia immagine ogni 2 secondi
+}
+
+// Slideshow Tetti
+let slideIndexTetti = 0;
+showSlidesTetti();
+
+function showSlidesTetti() {
+    let slides = document.getElementsByClassName("mySlidesTetti");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexTetti++;
+    if (slideIndexTetti > slides.length) {
+        slideIndexTetti = 1;
+    }
+    slides[slideIndexTetti - 1].style.display = "block";
+    setTimeout(showSlidesTetti, 2000); // Cambia immagine ogni 2 secondi
+}
+
+// Slideshow Carpenteria
+let slideIndexCarpenteria = 0;
+showSlidesCarpenteria();
+
+function showSlidesCarpenteria() {
+    let slides = document.getElementsByClassName("mySlidesCarpenteria");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndexCarpenteria++;
+    if (slideIndexCarpenteria > slides.length) {
+        slideIndexCarpenteria = 1;
+    }
+    slides[slideIndexCarpenteria - 1].style.display = "block";
+    setTimeout(showSlidesCarpenteria, 2000); // Cambia immagine ogni 2 secondi
+}
+
+// Funzione per l'accettazione dei cookie
+function acceptCookies() {
+    document.getElementById('cookie-consent').style.display = 'none';
+    // Altre azioni da eseguire dopo l'accettazione dei cookie, se necessario
 }
